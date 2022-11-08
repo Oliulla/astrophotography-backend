d5855fd8eed72884787edf893bcdc15b6de5d84f
Oliulla <razasm29@gmail.com>
2022-11-08 12:46:36 +0600

    connect to mongodb

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+
+// mognodb admin
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.g9drewa.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function dbConnect() {
+
+    client.connect(err => {
+        // check wheather db connect or not
+        if(err) {
+            console.log('database connected faild')
+        }
+        console.log('database connected');
+
+        try {
+            
+        } catch (error) {
+            
+        }
+    });
+}
+dbConnect();
+
+
+// check starting server 
 app.get('/', (req, res) => {
     try {
         res.send({
